03ad4fc5800cc583282151da4c5ddcbbc6e2607f
Crowdbotics <team@crowdbotics.com>
2023-08-18 22:25:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-dream-42937",
+  url: "https://silent-dream-42937.botics.co",
+  api: "https://silent-dream-42937.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
